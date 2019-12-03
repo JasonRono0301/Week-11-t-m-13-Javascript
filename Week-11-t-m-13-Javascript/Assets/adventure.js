a88a69b555af7feeb7673080220821cc023653a6
Jason <99057964@mydavinci.nl>
2019-12-03 08:42:09 +0100

    Nog bezig
    Ben bezig met het maken van de adventure game.

--- Week-11-t-m-13-Javascript/Assets/adventure.js
+++ Week-11-t-m-13-Javascript/Assets/adventure.js
@@ -55,39 +55,83 @@
         button2.style.display = "inline-block";
         button3.style.display = "inline-block";
 
-      
+        button1.setAttribute("onclick", "level2()");
+       
+        
+    
+        
+        
+
     }
     
-    //Gameover function
-  
-
-
-
+    
+    //function level 2.
     function level2(){
         document.body.style.backgroundImage = "url(Assets/RuimteVh/RuimteGang.jpg)";
 
         titel.innerHTML = "Level 2";
-        uitleg.innerHTML = "Je bent door de deur heen en je bevindt je nu in een donkere gang met een andere deur aan de overkant maar je ziet dat je een swipe card ervoor nodig hebt.<br> De swipe card zit in 1 van de 3 kamer rechts van je.<br><br>Welke deur ga je kiezen? ";
+        uitleg.innerHTML = "Je bent door de deur heen en je bevindt je nu in een lichte gang met een deur aan het eind maar je ziet dat je een swipe card ervoor nodig hebt.<br> De swipe card zit in 1 van de 3 kamer rechts van je als je de swipe card hebt wordt je automatisch doorgestuurd naar de volgende level.<br><br>Welke deur ga je kiezen? ";
         
-        var lvl2btn1 = button1;
-        var lvl2btn2 = button2;
-        var lvl2btn3 = button3;
+        button1.style.display = "inline-block";
+        button2.style.display = "inline-block";
+        button3.style.display = "inline-block";
 
-        lvl2btn2.style.display = "inline-block";
-        lvl2btn3.style.display = "inline-block";
+
+
+        button1.innerHTML = "Kamer 1";
+        button2.innerHTML = "Kamer 2";
+        button3.innerHTML = "Kamer 3";
+
+        document.getElementById("button1").setAttribute("onclick","wrongroom()");
+        button2.setAttribute("onclick","Goedekamer()");
+        document.getElementById("button3").setAttribute("onclick","wrongroom()");
         
-        lvl2btn1.innerHTML = "Kamer 1";
-        lvl2btn2.innerHTML = "Kamer 2";
-        lvl2btn3.innerHTML = "Kamer 3";
+
 
         
     }
 
+
+    //Function voor de verkeerdekamer.
+    function wrongroom(){
+        document.body.style.backgroundImage = "url(Assets/RuimteVh/wrong.jpg)";
+        title.innerHTML = "Verkeerd antwoord";
+        uitleg.style.display = "none";
+        
+        button1.style.display = "none";
+
+        button2.innerHTML = "Ga terug";
+        document.getElementById("button2").setAttribute("onclick","level2()");
+
+        button3.style.display = "none";
+    
+    }
+    
+    //Function voor de goedekamer.
+    function Goedekamer(){
+        document.body.style.backgroundImage = "url(Assets/RuimteVh/DonkereKamer.jpg)";
+
+        title.innerHTML = "Goede kamer";
+        uitleg.innerHTML = "Je bent in de goede kamer gekomen en je hebt de swipe kaart gevonden.<br><br> Ga terug naar de gang zodat je je deur kan openmaken. ";
+        
+        button1.innerHTML = "Naar volgende level";
+        document.getElementById("button1").setAttribute("onclick","level3()");
+
+        button2.style.display = "none";
+        button3.style.display = "none";
+    }
     
 
+    //Function voor level 3.
+    function level3(){
+        document.body.style.backgroundImage = "url(Assets/RuimteVh/Obstakel.jpg)";
 
-    
+        title.innerHTML = "Level 3";
+        uitleg.innerHTML = "Het is je gelukt om door de gesloten deur heen te gaan. <br><br> Maar uit het niets komt er een kleine asteroïde aan op je shuttle waardoor er schade is geleverd op de plek waar je nu bent. Je ziet dat er vuur komt uit muur naast je dus je zit in een obstakel.<br><br> Wat ga je nu doen?<br><br>De brandblusser naast je pakken of overheen springen?";
 
-
+        button1.innerHTML = "Brandblusser";
+        button2.innerHTML = "Overheen springen";
+        button3.style.display = "none";
+    }
     
     
