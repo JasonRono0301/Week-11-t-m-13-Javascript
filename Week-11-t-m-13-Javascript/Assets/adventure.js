31afd5e0d99e387da38e19862309aa98da7ff881
Jason <99057964@mydavinci.nl>
2019-12-03 11:42:35 +0100

    Nog bezig
    Nog bezig met maken van de game

--- Week-11-t-m-13-Javascript/Assets/adventure.js
+++ Week-11-t-m-13-Javascript/Assets/adventure.js
@@ -5,38 +5,39 @@
 //Alle id namen:
 //title //description //button1 //button2 //button3 //inventoryItem
 
-    //VARS van de: document.getElementById(ID-NAME).
-    var titel = document.getElementById("title");
-    var uitleg = document.getElementById("description");
-    var button1 = document.getElementById("button1");
-    var button2 = document.getElementById("button2");
-    var button3 = document.getElementById("button3");
-    var inventory = document.getElementById("inventoryItem");
+    //Alle document.getElementById(ID-NAME).
+    //document.getElementById("title");
+    //document.getElementById("description");
+    //document.getElementById("button1");
+    //document.getElementById("button2");
+    //document.getElementById("button3");
+    //document.getElementById("inventoryItem");
 
     //functions per level.
         
     //Intro level.
     document.body.style.backgroundImage = "url(Assets/RuimteVh/Intro.jpg)";
-    title.innerText = "Welkom by mijn spel";
-    uitleg.innerText = "Druk op de knop om het spel te starten"
-    uitleg.style.paddingBottom = "10px"    
-    button2.style.display = "none";
-    button3.style.display = "none";
-    inventory.style.display = "none";
-    button1.innerHTML = "Start"
+    document.getElementById("title").innerHTML = "Welkom by mijn spel";
+    document.getElementById("description").innerHTML = "Druk op de knop om het spel te starten"
+    document.getElementById("button1").style.paddingBottom = "0px"    
+    document.getElementById("button2").style.display = "none";
+    document.getElementById("button3").style.display = "none";
+    document.getElementById("inventoryItem").style.display = "none";
+    document.getElementById("button1").innerHTML = "Start"
     
     
     
 
     //Begin verhaal.
-    button1.setAttribute('onclick','Intro()');
+    document.getElementById("button1").setAttribute("onclick","Intro()");
 
+    
     function Intro(){
         document.body.style.backgroundImage = "url(Assets/RuimteVh/Intro1.jpg)";
-        titel.innerHTML = "Intro Verhaal";
-        uitleg.innerHTML = "Je zit in een shuttle die onderweg is naar een zwartgat en jou taak is om de shuttle terug te sturen naar Aarde maar de motor doen het niet.<br><br> Dus je moet in de shuttle zelf zoeken naar benodigheden om de motor te laten starten. <br><br> SUCCES!<br>";
-        button1.innerHTML = "Druk om te beginnen"
-        button1.setAttribute("onclick","level1()");
+        document.getElementById("title").innerHTML = "Intro Verhaal";
+        document.getElementById("description").innerHTML = "Je zit in een shuttle die onderweg is naar een zwartgat en jou taak is om de shuttle terug te sturen naar Aarde maar de motor doen het niet.<br><br> Dus je moet in de shuttle zelf zoeken naar benodigheden om de motor te laten starten. <br><br> SUCCES!<br>";
+        document.getElementById("button1").innerHTML = "Druk om te beginnen"
+        document.getElementById("button1").setAttribute("onclick","level1()");
     }
     
 
@@ -44,24 +45,18 @@
     function level1(){
         document.body.style.backgroundImage = "url(Assets/RuimteVh/Shuttle.jpg)";
     
-        title.innerHTML = "Level 1";
-        uitleg.innerHTML = "Je zit in de cockpit van de shuttle zelf en achter je zit de deur op slot je kunt de deur openen door 1 van de knoppen voor je te drukken waardoor de deur achter je open gaat. <br><br> 1 knop maakt de deur open en de andere 2 laat het schip ontploffen. <br><br> Welke knop ga je drukken?";
+        document.getElementById("title").innerHTML = "Level 1";
+        document.getElementById("description").innerHTML = "Je zit in de cockpit van de shuttle zelf en achter je zit de deur op slot je kunt de deur openen door 1 van de knoppen voor je te drukken waardoor de deur achter je open gaat. <br><br> 1 knop maakt de deur open en de andere 2 laat het schip ontploffen. <br><br> Welke knop ga je drukken?";
 
-        button1.innerHTML = "Knop 1";
-        button2.innerHTML = "Knop 2";
-        button3.innerHTML = "Knop 3";
+        document.getElementById("button1").innerHTML = "Knop 1";
+        document.getElementById("button2").innerHTML = "Knop 2";
+        document.getElementById("button3").innerHTML = "Knop 3";
 
-        button1.style.display = "inline-block";
-        button2.style.display = "inline-block";
-        button3.style.display = "inline-block";
+        document.getElementById("button1").style.display = "inline-block";
+        document.getElementById("button2").style.display = "inline-block";
+        document.getElementById("button3").style.display = "inline-block";
 
-        button1.setAttribute("onclick", "level2()");
-       
-        
-    
-        
-        
-
+        document.getElementById("button1").setAttribute("onclick", "level2()");
     }
     
     
@@ -69,69 +64,62 @@
     function level2(){
         document.body.style.backgroundImage = "url(Assets/RuimteVh/RuimteGang.jpg)";
 
-        titel.innerHTML = "Level 2";
-        uitleg.innerHTML = "Je bent door de deur heen en je bevindt je nu in een lichte gang met een deur aan het eind maar je ziet dat je een swipe card ervoor nodig hebt.<br> De swipe card zit in 1 van de 3 kamer rechts van je als je de swipe card hebt wordt je automatisch doorgestuurd naar de volgende level.<br><br>Welke deur ga je kiezen? ";
+        document.getElementById("title").innerHTML = "Level 2";
+        document.getElementById("description").innerHTML = "Je bent door de deur heen en je bevindt je nu in een lichte gang met een deur aan het eind maar je ziet dat je een swipe card ervoor nodig hebt.<br> De swipe card zit in 1 van de 3 kamer rechts van je als je de swipe card hebt wordt je automatisch doorgestuurd naar de volgende level.<br><br>Welke deur ga je kiezen? ";
         
-        button1.style.display = "inline-block";
-        button2.style.display = "inline-block";
-        button3.style.display = "inline-block";
+        document.getElementById("button1").style.display = "inline-block";
+        document.getElementById("button2").style.display = "inline-block";
+        document.getElementById("button3").style.display = "inline-block";
 
 
+        document.getElementById("button1").innerHTML = "Kamer 1";
+        document.getElementById("button2").innerHTML = "Kamer 2";
+        document.getElementById("button3").innerHTML = "Kamer 3";
 
-        button1.innerHTML = "Kamer 1";
-        button2.innerHTML = "Kamer 2";
-        button3.innerHTML = "Kamer 3";
 
         document.getElementById("button1").setAttribute("onclick","wrongroom()");
-        button2.setAttribute("onclick","Goedekamer()");
+        document.getElementById("button2").setAttribute("onclick","level3()");
         document.getElementById("button3").setAttribute("onclick","wrongroom()");
         
-
-
+        
         
     }
 
 
-    //Function voor de verkeerdekamer.
-    function wrongroom(){
-        document.body.style.backgroundImage = "url(Assets/RuimteVh/wrong.jpg)";
-        title.innerHTML = "Verkeerd antwoord";
-        uitleg.style.display = "none";
-        
-        button1.style.display = "none";
-
-        button2.innerHTML = "Ga terug";
-        document.getElementById("button2").setAttribute("onclick","level2()");
-
-        button3.style.display = "none";
-    
-    }
-    
-    //Function voor de goedekamer.
-    function Goedekamer(){
-        document.body.style.backgroundImage = "url(Assets/RuimteVh/DonkereKamer.jpg)";
-
-        title.innerHTML = "Goede kamer";
-        uitleg.innerHTML = "Je bent in de goede kamer gekomen en je hebt de swipe kaart gevonden.<br><br> Ga terug naar de gang zodat je je deur kan openmaken. ";
-        
-        button1.innerHTML = "Naar volgende level";
-        document.getElementById("button1").setAttribute("onclick","level3()");
-
-        button2.style.display = "none";
-        button3.style.display = "none";
-    }
-    
-
-    //Function voor level 3.
+   //Function voor level 3.
     function level3(){
         document.body.style.backgroundImage = "url(Assets/RuimteVh/Obstakel.jpg)";
 
-        title.innerHTML = "Level 3";
-        uitleg.innerHTML = "Het is je gelukt om door de gesloten deur heen te gaan. <br><br> Maar uit het niets komt er een kleine asteroïde aan op je shuttle waardoor er schade is geleverd op de plek waar je nu bent. Je ziet dat er vuur komt uit muur naast je dus je zit in een obstakel.<br><br> Wat ga je nu doen?<br><br>De brandblusser naast je pakken of overheen springen?";
+        document.getElementById("title").innerHTML = "Level 3";
+        document.getElementById("description").innerHTML = "Het is je gelukt om door de gesloten deur heen te gaan. <br><br> Maar uit het niets komt er een kleine asteroïde aan op je shuttle waardoor er schade is geleverd op de plek waar je nu bent. Je ziet dat er vuur komt uit muur naast je dus je zit in een obstakel.<br><br> Wat ga je nu doen?<br><br>De brandblusser naast je pakken of overheen springen?";
+        document.getElementById("description").style.paddingBottom = "10px";
+       var lvl3btn1 =  document.getElementById("button1").innerHTML = "Brandblusser";
+       var lvl3btn2 =  document.getElementById("button2").innerHTML = "Overheen springen";
+       var lvl3btn3 = document.getElementById("button3").style.display = "none";
 
-        button1.innerHTML = "Brandblusser";
-        button2.innerHTML = "Overheen springen";
-        button3.style.display = "none";
+       if(level3 == lvl3btn1){
+           lvl3btn1 = true;
+            alert("Het antwoord is goed om het vuur te blussen je word nu doorgestuurd naar het volgende level.");
+             document.getElementById("button1").setAttribute("onclick","level4()");
+       } else {
+           lvl3btn1 = false;
+           alert("Je antwoord is fout kies een andere manier.");
+       }
+    
     }
     
+    //Function voor level 4.
+    function level4(){
+        document.body.style.backgroundImage = "url(Assets/RuimteVh/Alien.jpg)";
+
+        document.getElementById("title").innerHTML = "Level 4";
+        document.getElementById("description").innerHTML = "Je bent door je obstakel heen gekomen. Maar je ziet dat er een asteriode in het shuttle zit je hoort krak en ziet iets eruit bewegen het is een grote alien die eruit is gekomen. <br><br> Wat ga je doen?<br><br>";
+
+       var lvl4b1 = document.getElementById("button1").innerHTML = "Wegrennen";
+       var lvl4b2 = document.getElementById("button2").innerHTML = "Het neersteken met je zakmes";
+       var lvl4b3 = document.getElementById("button3").innerHTML = "Kamer rechts van je ingaan";
+        
+       
+        
     
+    }
